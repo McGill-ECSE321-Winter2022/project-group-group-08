8043747f47061b41b13b3b492738685a062ffa1d
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2022-02-15 02:08:09 -0500

    Revert "Update GroceryStoreBackendApplicationTests.java"
    This reverts commit 8ceea5af58e0f95150dd8a7d4b142b511e07d45f.

--- GroceryStore-Backend/src/test/java/mcgill/ecse321/GroceryStoreBackend/GroceryStoreBackendApplicationTests.java
+++ GroceryStore-Backend/src/test/java/mcgill/ecse321/GroceryStoreBackend/GroceryStoreBackendApplicationTests.java
@@ -1,12 +1,12 @@
 package mcgill.ecse321.GroceryStoreBackend;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest
+ import org.junit.jupiter.api.Test;
+ import org.springframework.boot.test.context.SpringBootTest;
+
+ @SpringBootTest
 class GroceryStoreBackendApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
 	}
 
