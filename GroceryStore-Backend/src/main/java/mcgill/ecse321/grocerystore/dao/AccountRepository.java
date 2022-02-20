2e738c11e8997e9bf35ff54865d26667076b2a9c
aigiarnc <aigiarn.cheuk@gmail.com>
2022-02-20 18:35:29 -0500

    AccountRepository class

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/AccountRepository.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/AccountRepository.java
@@ -1,5 +1,7 @@
 package mcgill.ecse321.grocerystore.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import mcgill.ecse321.grocerystore.model.Account;
@@ -7,4 +9,16 @@ import mcgill.ecse321.grocerystore.model.Account;
 public interface AccountRepository extends CrudRepository<Account, String>{
 
 	Account findAccountByUsername(String username);
+	
+	List<Account> findAccountByUsernameIgnoreCase(String username);
+	
+	List<Account> findAccountByUsernameContaining(String username);
+	
+	List<Account> findAccountByInTown(boolean inTown);
+	
+	List<Account> findAccountByTotalPointsBetween(int minPoint, int maxPoint);
+	
+	List<Account> findAccountByInTownAndTotalPointsBetween(boolean inTown, int minPoint, int maxPoint);
+	
 }
+	
