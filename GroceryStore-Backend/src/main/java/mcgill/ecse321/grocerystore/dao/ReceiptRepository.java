570b7648bc6abbc4436fb7eb7069f87faf00e744
Mihail C <68878155+MihailOfficial@users.noreply.github.com>
2022-02-21 10:50:12 -0500

    specified receipt crud

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/ReceiptRepository.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dao/ReceiptRepository.java
@@ -1,10 +1,20 @@
 package mcgill.ecse321.grocerystore.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import mcgill.ecse321.grocerystore.model.Receipt;
+import mcgill.ecse321.grocerystore.model.Receipt.ReceiptStatus;
+import mcgill.ecse321.grocerystore.model.Receipt.ReceiptType;
 
 public interface ReceiptRepository extends CrudRepository<Receipt, Integer>{
 	
-	Receipt findReceiptByReceiptNum(int orderNum);
+	Receipt findReceiptByReceiptNum(int receiptNum);
+	
+	List<Receipt> findReceiptByReceiptStatus(ReceiptStatus receiptStatus);
+	
+	List<Receipt> findReceiptByReceiptType(ReceiptType receiptType);
+	
+	List<Receipt> findReceiptByReceiptStatusAndReceiptType(ReceiptStatus receiptStatus, ReceiptType receiptType);
 }
