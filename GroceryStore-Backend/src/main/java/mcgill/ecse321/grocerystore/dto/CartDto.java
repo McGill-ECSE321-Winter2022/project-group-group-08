6da4bd314d7aefef877cb66df59427fc19a60003
Hongfei Liu <hongfei.liu@mail.mcgill.ca>
2022-03-08 19:43:31 -0500

    hhh

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dto/CartDto.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dto/CartDto.java
@@ -1,5 +1,32 @@
 package mcgill.ecse321.grocerystore.dto;
 
+import java.sql.Date;
+
 public class CartDto {
 
+    private int id;
+    private Date date;
+	private AccountDto account;
+	
+	public CartDto() {
+		
+	}
+
+	public CartDto(int id, Date date, AccountDto account) {
+		this.id = id;
+		this.date = date;
+		this.account = account;
+	}
+		
+	public int getId() {
+		return id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public AccountDto getAccount() {
+		return account;
+	}
 }
