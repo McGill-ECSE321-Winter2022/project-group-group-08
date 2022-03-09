3562ab399118e99e0393ab22ec2cb3b8f32dbc1f
selsshelf <89438543+selsshelf@users.noreply.github.com>
2022-03-08 19:26:18 -0500

    grocery store manager service

---
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/service/ManagerService.java
@@ -0,0 +1,33 @@
+package mcgill.ecse321.grocerystore.service;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import mcgill.ecse321.grocerystore.dao.UserRoleRepository;
+import mcgill.ecse321.grocerystore.model.UserRole;
+
+@Service
+public class ManagerService {
+	
+	@Autowired
+	UserRoleRepository userRoleRepository;
+	
+	@Transactional 
+	public UserRole createManager() {
+		Manager manager = new Manager();
+		return manager;
+	}
+	
+	public UserRole getManager(int id) {
+		UserRole managerID = userRoleRepository.findUserRoleById(id);
+		return managerID;
+	}
+	 	
+
+}
