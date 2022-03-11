f714ea63d15d95b87f0cb286d507c68d75550b2e
selsshelf <89438543+selsshelf@users.noreply.github.com>
2022-03-10 23:54:37 -0500

    fix

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dto/ManagerDto.java
+++ GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/dto/ManagerDto.java
@@ -4,11 +4,14 @@ import mcgill.ecse321.grocerystore.model.UserRole;
 
 public class ManagerDto {
 	
-	private int id;
 	private UserRoleDto userRole;
 	
 	public ManagerDto() {
 		
+	}
+	
+	public ManagerDto(UserRoleDto userRole) {
+		this.userRole = userRole;
 	}
 	
 	public UserRoleDto getManager() {
