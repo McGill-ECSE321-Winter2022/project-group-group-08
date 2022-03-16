dbea1ea2667901d2ef5fd1384e9cfe431db09100
aigiarnc <aigiarn.cheuk@gmail.com>
2022-03-16 16:14:53 -0400

    TestCustomerService 88% coverage

--- GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/service/TestCustomerService.java
+++ GroceryStore-Backend/src/test/java/mcgill/ecse321/grocerystore/service/TestCustomerService.java
@@ -45,23 +45,30 @@ public class TestCustomerService {
 	private PersonService personService;
 
 	private static final int ID_KEY = 1234567;
+	private static final int INVALID_ID_KEY = -2;
 	private static final int FAKE_ID_KEY = 6666666;
+
 	private static final TierClass TIER_KEY = TierClass.Bronze;
 	private static final TierClass NEW_TIER_KEY = TierClass.Silver;
 	private static final TierClass INVALID_TIER_KEY = null;
+
 	private static final boolean BAN_KEY = false;
 	private static final boolean NEW_BAN_KEY = true;
+
 	private static final String EMAIL_KEY = "email@gmail.com";
 	private static final String FIRSTNAME_KEY = "Bob";
 	private static final String LASTNAME_KEY = "The Builder";
 	private static final String PHONE_KEY = "111-222-3333";
 	private static final String ADDR_KEY = "123 street";
+	
 
 	@BeforeEach
 	public void setMockOutput() {
 		lenient().when(customerDao.findCustomerById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
 			if (invocation.getArgument(0).equals(ID_KEY)) {
 				Customer customer = new Customer();
+				Person person = new Person();
+				customer.setPerson(person);
 				customer.setId(ID_KEY);
 				customer.setTierclass(TIER_KEY);
 				customer.setBan(BAN_KEY);
@@ -86,6 +93,12 @@ public class TestCustomerService {
 			customerByBanList.add(customer);
 			return customerByBanList;
 		});
+		Person test_person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY, ADDR_KEY);
+		lenient().when(userRoleDao.findUserRoleByPerson(test_person)).thenAnswer((InvocationOnMock invocation) -> {
+			Customer customer = new Customer();
+			customer.setPerson(test_person);
+			return customer;
+		});
 		lenient().when(customerDao.existsById(ID_KEY)).thenReturn(true);
 		lenient().when(personDao.existsById(anyString())).thenReturn(true);
 		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
@@ -100,8 +113,7 @@ public class TestCustomerService {
 		TierClass defaultTier = TierClass.Bronze;
 		boolean defaultBan = false;
 		Customer customer = null;
-		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY,
-				ADDR_KEY);
+		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY, ADDR_KEY);
 		try {
 			customer = service.createCustomer(person);
 		} catch (IllegalArgumentException e) {
@@ -114,7 +126,7 @@ public class TestCustomerService {
 	}
 
 	@Test
-	public void testCreateCustomerSimpleInvalidPerson() {
+	public void testCreateCustomerNonexistentPerson() {
 		String error = null;
 		Customer customer = null;
 		Person person = null;
@@ -127,12 +139,11 @@ public class TestCustomerService {
 		assertNull(person);
 		assertEquals("Invalid person", error);
 	}
-	
+
 	@Test
 	public void testCreateCustomer() {
 		Customer customer = null;
-		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY,
-				ADDR_KEY);
+		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY, ADDR_KEY);
 		try {
 			customer = service.createCustomer(person, TIER_KEY, BAN_KEY);
 		} catch (IllegalArgumentException e) {
@@ -142,7 +153,7 @@ public class TestCustomerService {
 		assertEquals(TIER_KEY, customer.getTierclass());
 		assertEquals(BAN_KEY, customer.getBan());
 	}
-	
+
 	@Test
 	public void testCreateCustomerInvalidPerson() {
 		String error = null;
@@ -157,15 +168,12 @@ public class TestCustomerService {
 		assertNull(person);
 		assertEquals("Invalid person", error);
 	}
-	
-	@SuppressWarnings("unused")
+
 	@Test
 	public void testCreateCustomerInvalidPersonWithRole() {
 		String error = null;
 		Customer customer = null;
-		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY,
-				ADDR_KEY);
-		UserRole oldCustomer = service.createCustomer(person, TIER_KEY, BAN_KEY);
+		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY, ADDR_KEY);;
 		try {
 			customer = service.createCustomer(person, TIER_KEY, BAN_KEY);
 		} catch (InvalidInputException e) {
@@ -174,13 +182,12 @@ public class TestCustomerService {
 		assertNull(customer);
 		assertEquals("Person has already been assigned a role", error);
 	}
-	
+
 	@Test
 	public void testCreateCustomerInvalidTier() {
 		String error = null;
 		Customer customer = null;
-		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY,
-				ADDR_KEY);
+		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY, ADDR_KEY);
 		try {
 			customer = service.createCustomer(person, INVALID_TIER_KEY, BAN_KEY);
 		} catch (InvalidInputException e) {
@@ -189,29 +196,27 @@ public class TestCustomerService {
 		assertNull(customer);
 		assertEquals("Invalid tier Class", error);
 	}
-	
-//	@Test
-//	public void testUpdateCustomer() {
-//		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY,
-//				ADDR_KEY);
-//		Customer customer = service.createCustomer(person, TIER_KEY, BAN_KEY);
-//		int id = customer.getId();
-//		try {
-//			customer = service.updateCustomer(id, person, NEW_TIER_KEY, NEW_BAN_KEY);
-//		} catch (IllegalArgumentException e) {
-//			fail();
-//		}
-//		assertNotNull(customer);
-//		assertEquals(NEW_TIER_KEY, customer.getTierclass());
-//		assertEquals(NEW_BAN_KEY, customer.getBan());
-//	}
+
+	@Test
+	public void testUpdateCustomer() {
+		Customer customer = null;
+		Person person = personService.createPerson(EMAIL_KEY, FIRSTNAME_KEY, LASTNAME_KEY, PHONE_KEY, ADDR_KEY);;
+		try {
+			customer = service.updateCustomer(ID_KEY, person, NEW_TIER_KEY, NEW_BAN_KEY);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		assertNotNull(customer);
+		assertEquals(NEW_TIER_KEY, customer.getTierclass());
+		assertEquals(NEW_BAN_KEY, customer.getBan());
+	}
 
 	@Test
 	public void testGetExistingCustomer() {
-		Customer customer  = null;
+		Customer customer = null;
 		try {
 			customer = service.getCustomer(ID_KEY);
-		}catch(InvalidInputException e) {
+		} catch (InvalidInputException e) {
 			fail();
 		}
 		assertNotNull(customer);
@@ -220,16 +225,29 @@ public class TestCustomerService {
 	}
 
 	@Test
-	public void testGetNonExistingCustomer() {
-		Customer customer  = null;
+	public void testGetCustomerNegativeID() {
 		String error = "";
+		Customer customer = null;
 		try {
-			customer = service.getCustomer(FAKE_ID_KEY);
-		}catch(InvalidInputException e) {
+			customer = service.getCustomer(INVALID_ID_KEY);
+		} catch (InvalidInputException e) {
 			error = e.getMessage();
 		}
 		assertNull(customer);
-		assertEquals("Customer does not exist",error);
+		assertEquals("Invalid id", error);
+	}
+
+	@Test
+	public void testGetNonExistingCustomer() {
+		Customer customer = null;
+		String error = "";
+		try {
+			customer = service.getCustomer(FAKE_ID_KEY);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		assertNull(customer);
+		assertEquals("Customer does not exist", error);
 	}
 
 	@Test
@@ -237,7 +255,7 @@ public class TestCustomerService {
 		List<Customer> customers = new ArrayList<Customer>();
 		try {
 			customers = service.getAllCustomerByTier(TIER_KEY);
-		}catch(InvalidInputException e){
+		} catch (InvalidInputException e) {
 			fail();
 		}
 		Customer customer = customers.get(0);
@@ -247,11 +265,26 @@ public class TestCustomerService {
 	}
 
 	@Test
+	public void testGetAllCustomerByTierInvalidTier() {
+		String error = null;
+		List<Customer> customers = new ArrayList<Customer>();
+		Customer customer = null;
+		try {
+			customers = service.getAllCustomerByTier(INVALID_TIER_KEY);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		assertNull(customer);
+		assertEquals(0, customers.size());
+		assertEquals("Invalid tier Class", error);
+	}
+
+	@Test
 	public void testGetAllCustomerByBan() {
 		List<Customer> customers = new ArrayList<Customer>();
 		try {
 			customers = service.getAllCustomerByBan(BAN_KEY);
-		}catch(InvalidInputException e){
+		} catch (InvalidInputException e) {
 			fail();
 		}
 		Customer customer = customers.get(0);
@@ -271,4 +304,31 @@ public class TestCustomerService {
 		}
 		assertNotNull(customer);
 	}
+
+	@Test
+	public void testDeleteCustomerNegativeID() {
+		String error = null;
+		Customer customer = null;
+		try {
+			customer = service.deleteCustomer(INVALID_ID_KEY);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		assertNull(customer);
+		assertEquals("Invalid id", error);
+	}
+
+	@Test
+	public void testDeleteCustomerNonexistentCustomer() {
+		String error = null;
+		Customer customer = null;
+		try {
+			customer = service.deleteCustomer(FAKE_ID_KEY);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		assertNull(customer);
+		assertEquals("Customer does not exist", error);
+	}
+
 }
