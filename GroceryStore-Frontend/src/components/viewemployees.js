73fb818635f94baa3faab1a3b3f2479b64d42aea
aigiarnc <aigiarn.cheuk@gmail.com>
2022-04-02 18:19:05 -0400

    Update viewemployees.js

--- GroceryStore-Frontend/src/components/viewemployees.js
+++ GroceryStore-Frontend/src/components/viewemployees.js
@@ -36,8 +36,50 @@ export default {
   },
 
   methods: {
+    getEmployeeByID(employeeID) {
+      AXIOS.get("/employee/".concat(employeeID))
+        .then(response => {
+          this.employees = response.data;
+        })
+        .catch(e => {
+          this.errorEmployees = e;
+        });
+    },
+    getEmployeeByName(firstName, lastName) {
+      if (firstName == undefined) {
+        firstName = "";
+      }
+      if (lastName == undefined) {
+        lastName = "";
+      }
+      AXIOS.get("/getPersonsByFirstNameLastNameContainingIgnoreCase/", {
+        params: {
+          firstName: firstName,
+          lastName: lastName
+        }
+      })
+        .then(response => {
+          this.employees = response.data;
+          console.log(response.data);
+        })
+        .catch(error => {
+          var errorMsg = error;
+          if (error.response) {
+            errorMsg = error.response.data;
+          }
+        });
+    },
+    getEmployeeByEmail(email) {
+      AXIOS.get("/employee/email/".concat(email))
+        .then(response => {
+          this.employees = response.data;
+        })
+        .catch(e => {
+          this.errorEmployees = e;
+        });
+    },
     fireEmployee: function(employeeID) {
-      AXIOS.delete("/employee/delete".concat(employeeID)).catch(e => {
+      AXIOS.delete("/employee/delete/".concat(employeeID)).catch(e => {
         var error = e.response.data.message;
         console.log(error);
       });
