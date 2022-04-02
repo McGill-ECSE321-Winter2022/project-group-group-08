e6e343d63190680475d1fb1c43e5c53d75189499
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2022-04-01 21:45:04 -0400

    update index.js

--- GroceryStore-Frontend/src/router/index.js
+++ GroceryStore-Frontend/src/router/index.js
@@ -1,43 +1,32 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Hello from '@/components/Hello';
-import Item from '@/components/Item.vue';
-import Browse from '@/components/Browse.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "@/components/Hello.vue";
+import Item from "@/components/Item.vue";
+import Browse from "@/components/Browse.vue";
 // import ViewShift from '@/components/ViewShift';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: "/cart",
-      name: "Cart",
-      component : Cart
-    },
-    {
-      path: "/browse",
-      name: "Browse",
-      component : Browse
-    },
-    {
-      path: "/item",
-      name: "Item",
-      component : Item
-    },
-    {
-      path: "/quantity",
-      name: "Quantity",
-      component : Quantity
-    }
-    // {
-    //   path: "/ViewShifts",
-    //   name: "ViewShifts",
-    //   component : ViewShift
-    // }
-  ]
-})
+    routes: [{
+            path: "/",
+            name: "Hello",
+            component: Hello
+        },
+        {
+            path: "/browse",
+            name: "Browse",
+            component: Browse
+        },
+        {
+            path: "/item",
+            name: "Item",
+            component: Item
+        }
+        // {
+        //   path: "/ViewShifts",
+        //   name: "ViewShifts",
+        //   component : ViewShift
+        // }
+    ]
+});
