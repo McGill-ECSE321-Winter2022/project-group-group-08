4f700bf758ea0feb4b6b0988678ebf80fcb6acd4
aigiarnc <aigiarn.cheuk@gmail.com>
2022-04-03 14:36:38 -0400

    comments browse.vue

--- GroceryStore-Frontend/src/components/Browse.vue
+++ GroceryStore-Frontend/src/components/Browse.vue
@@ -1,26 +1,22 @@
 <template>
-
-   
-           
-                <div class="grid-container" style = "width: 100%;">
-                    <Item 
-                    v-for="item in items" :key="item.name" 
-                    :curId="item.id"
-                    :itemImage="item.image"
-                    :itemName="item.name"
-                    :itemPrice="item.price"
-                    :validUser="validUser"
-                    ></Item>
-                
-                </div>
-                
-      
+  <div class="grid-container" style="width: 100%">
+    <!--View Items Grid -->
+    <Item
+      v-for="item in items"
+      :key="item.name"
+      :curId="item.id"
+      :itemImage="item.image"
+      :itemName="item.name"
+      :itemPrice="item.price"
+      :validUser="validUser"
+    ></Item>
+  </div>
 </template>
 <script src="./browse.js"></script>
 <style scoped>
 .grid-container {
-    width: 100%;
-    column-width: 300px;
-  display:inline-block;
+  width: 100%;
+  column-width: 300px;
+  display: inline-block;
 }
 </style>
