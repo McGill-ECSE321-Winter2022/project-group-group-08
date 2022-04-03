f7ceef2f23a13308cc3c7820f69bc05d8f5cc7af
aigiarnc <aigiarn.cheuk@gmail.com>
2022-04-03 14:25:09 -0400

    comments inventorymanagement.vue

--- GroceryStore-Frontend/src/components/InventoryManagement.vue
+++ GroceryStore-Frontend/src/components/InventoryManagement.vue
@@ -1,207 +1,230 @@
-<template> 
- <div class="d-flex align-items-center justify-content-center" style="height: 800px;" id = "InventoryManagement">
-
-  <table>
-    <h1> Inventory Management </h1>
-    <h4> Modify Items Attributes </h4> 
-    <v-data-table class="elevation-1">
-        <th>Item ID </th>
+<template>
+  <div
+    class="d-flex align-items-center justify-content-center"
+    style="height: 800px"
+    id="InventoryManagement"
+  >
+    <table>
+      <h1>Inventory Management</h1>
+      <h4>Modify Items Attributes</h4>
+      <!-- Data Table for Items -->
+      <v-data-table class="elevation-1">
+        <th>Item ID</th>
         <th>Name</th>
         <th>Image</th>
         <th>Price</th>
         <th>Point</th>
         <th>Return Policy</th>
         <th>Pickup</th>
-        <th>In Store Quantity </th>
+        <th>In Store Quantity</th>
         <th>Update Item</th>
         <th>Delete Item</th>
         <tbody>
-            <tr v-for="item in items" :key="item.id">
-                <td class="table-text">{{item.id}}</td>
-                <td>
-                    <input 
-                        style="margin-top: 6px;"
-                        type="text"
-                        v-model="itemName[item.id]"
-                        :placeholder="[[ item.name ]]"
-                    />
-                </td>
-                <td>
-                    <input 
-                        style="margin-top: 6px;"
-                        type="text"
-                        v-model="itemImage[item.id]"
-                        :placeholder="[[ item.image ]]"
-                    />
-                </td>
-                <td>
-                    <input 
-                        style="margin-top: 6px;"
-                        type="text"
-                        v-model="price[item.id]"
-                        :placeholder="[[ item.price ]]"
-                    />
-                </td>
-                <td>
-                    <input 
-                        style="margin-top: 6px;"
-                        type="text"
-                        v-model="point[item.id]"
-                        :placeholder="[[ item.point ]]"
-                    />
-                </td>
-                <td>
-                    <input 
-                        style="margin-top: 6px;"
-                        type="text"
-                        v-model="returnPolicy[item.id]"
-                        :placeholder="[[ item.returnPolicy ]]"
-                    />
-                </td>
-                <td>
-                    <input type="checkbox" id="pickup" v-model="item.pickup"/> 
-                </td>
-                <td>
-                    <input 
-                        style="margin-top: 6px;"
-                        type="text"
-                        v-model="inStoreQuantity[item.id]"
-                        :placeholder="[[ item.inStoreQuantity ]]"
-                    /> 
-                </td>
-                <td>
-                     <button
-                        class="btn btn-light"
-                        @click="updateItemAttributes(item.id, itemName[item.id], itemImage[item.id], price[item.id], point[item.id], returnPolicy[item.id], item.pickup, inStoreQuantity[item.id])"
-                    >
-                        Update
-                    </button>
-                </td>                                                                                                                          
-                <td>
-                    <button
-                        class="btn btn-light"
-                        @click="deleteItem(item.id);"
-                    >
-                        Delete
-                    </button>
-                </td>
-                
-                 
-            </tr> 
+          <tr v-for="item in items" :key="item.id">
+            <td class="table-text">{{ item.id }}</td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="itemName[item.id]"
+                :placeholder="[[item.name]]"
+              />
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="itemImage[item.id]"
+                :placeholder="[[item.image]]"
+              />
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="price[item.id]"
+                :placeholder="[[item.price]]"
+              />
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="point[item.id]"
+                :placeholder="[[item.point]]"
+              />
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="returnPolicy[item.id]"
+                :placeholder="[[item.returnPolicy]]"
+              />
+            </td>
+            <td>
+              <input type="checkbox" id="pickup" v-model="item.pickup" />
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="inStoreQuantity[item.id]"
+                :placeholder="[[item.inStoreQuantity]]"
+              />
+            </td>
+            <td>
+              <button
+                class="btn btn-light"
+                @click="
+                  updateItemAttributes(
+                    item.id,
+                    itemName[item.id],
+                    itemImage[item.id],
+                    price[item.id],
+                    point[item.id],
+                    returnPolicy[item.id],
+                    item.pickup,
+                    inStoreQuantity[item.id]
+                  )
+                "
+              >
+                Update
+              </button>
+            </td>
+            <td>
+              <button class="btn btn-light" @click="deleteItem(item.id)">
+                Delete
+              </button>
+            </td>
+          </tr>
         </tbody>
-    </v-data-table>
+      </v-data-table>
 
-        <div id = "addItemToStore"> 
-            <h4> Add Item to Inventory </h4>
-            <v-data-table>
-                <tr>
-                    <td>
-                        <p> Name: </p>
-                    </td>
-                    <td>
-                        <input
-                            style="margin-top: 6px;"
-                            type="text"
-                            v-model="newItemName"
-                            placeholder="Name"
-                        />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p> Image: </p>
-                    </td>
-                    <td>
-                        <input
-                            style="margin-top: 6px;"
-                            type="text"
-                            v-model="newItemImage"
-                            placeholder="Image"
-                        />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p> Price: </p>
-                    </td>
-                    <td>
-                        <input
-                            style="margin-top: 6px;"
-                            type="text"
-                            v-model="itemPrice"
-                            placeholder="Price"
-                        />
-                    </td>
-                </tr>
+    <!-- Add Item Component -->
+      <div id="addItemToStore">
+        <h4>Add Item to Inventory</h4>
+        <v-data-table>
+          <tr>
+            <td>
+              <p>Name:</p>
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="newItemName"
+                placeholder="Name"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Image:</p>
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="newItemImage"
+                placeholder="Image"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Price:</p>
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="itemPrice"
+                placeholder="Price"
+              />
+            </td>
+          </tr>
 
-                <tr>
-                    <td>
-                        <p> Point: </p>
-                    </td>
-                    <td>
-                        <input
-                            style="margin-top: 6px;"
-                            type="text"
-                            v-model="itemPoint"
-                            placeholder="Point"
-                        />
-                    </td>
-                </tr>
+          <tr>
+            <td>
+              <p>Point:</p>
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="itemPoint"
+                placeholder="Point"
+              />
+            </td>
+          </tr>
 
-                <tr>
-                    <td>
-                        <p> Return Policy: </p>
-                    </td>
-                    <td>
-                        <input
-                            style="margin-top: 6px;"
-                            type="text"
-                            v-model="itemReturnPolicy"
-                            placeholder="Return Policy"
-                        />
-                    </td>
-                </tr>
+          <tr>
+            <td>
+              <p>Return Policy:</p>
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="itemReturnPolicy"
+                placeholder="Return Policy"
+              />
+            </td>
+          </tr>
 
-                <tr>
-                    <td>
-                        <p> Available for pickup: </p>
-                    </td>
-                    <td>
-                        <input type="checkbox" id="pickup" v-model="itemPickup" placeholder= "Yes" /> 
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>
-                        <p> Quantity in Store: </p>
-                    </td>
-                    <td>
-                        <input
-                            style="margin-top: 6px;"
-                            type="text"
-                            v-model="itemInStoreQuantity"
-                            placeholder="In Store Quantity"
-                        />
-                    </td>
-                </tr>
-                <tr>
-                    <button
-                        class="btn btn-light"
-                        @click="addItemToInventory(newItemName,newItemImage, itemPrice, itemPoint, itemReturnPolicy, itemPickup, itemInStoreQuantity);"
-                    >
-                        Add Item
-                    </button>
-                </tr>
-            </v-data-table>
+          <tr>
+            <td>
+              <p>Available for pickup:</p>
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                id="pickup"
+                v-model="itemPickup"
+                placeholder="Yes"
+              />
+            </td>
+          </tr>
 
-           
-
-        </div>
+          <tr>
+            <td>
+              <p>Quantity in Store:</p>
+            </td>
+            <td>
+              <input
+                style="margin-top: 6px"
+                type="text"
+                v-model="itemInStoreQuantity"
+                placeholder="In Store Quantity"
+              />
+            </td>
+          </tr>
+          <tr>
+            <button
+              class="btn btn-light"
+              @click="
+                addItemToInventory(
+                  newItemName,
+                  newItemImage,
+                  itemPrice,
+                  itemPoint,
+                  itemReturnPolicy,
+                  itemPickup,
+                  itemInStoreQuantity
+                )
+              "
+            >
+              Add Item
+            </button>
+          </tr>
+        </v-data-table>
+      </div>
     </table>
-
- </div>
-
+  </div>
 </template>
 
-<style> </style>
-<script src="./inventoryManagement.js"> </script>
+<style>
+</style>
+<script src="./inventoryManagement.js">
+</script>
