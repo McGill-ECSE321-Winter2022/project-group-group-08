93dac25e46c57ab0f96cbca633c1b593cb6d5ec0
aigiarnc <aigiarn.cheuk@gmail.com>
2022-04-03 14:53:24 -0400

    Comments cart.vue

--- GroceryStore-Frontend/src/components/cart/Cart.vue
+++ GroceryStore-Frontend/src/components/cart/Cart.vue
@@ -1,8 +1,10 @@
 <template>
+    <!-- Show Cart Info -->
     <div id="cart">
         <Quantity 
             :cartId ="cart.id"
         ></Quantity>
+        <!-- Checkout Button -->
         <div class="checkout">
             <button class="button">Checkout and Pay</button>
         </div>
