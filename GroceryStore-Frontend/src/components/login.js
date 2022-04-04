001091de31f92a0a46b860e849278fef24bcf827
joeyxoay <zhi.koay@mail.mcgill.ca>
2022-04-03 23:02:35 -0400

    nav bar updates when login

--- GroceryStore-Frontend/src/components/login.js
+++ GroceryStore-Frontend/src/components/login.js
@@ -70,11 +70,48 @@ export default {
               username: sessionStorage.getItem("username")
             }
           })
-            .then(response => {
-              console.log(response.data);
-              sessionStorage.setItem("cartId", response.data.id);
-              this.$router.push({ path: `/Profile/${this.account.username}` });
-              location.reload();
+            .then(response2 => {
+              console.log(response2.data);
+              sessionStorage.setItem("cartId", response2.data.id);
+              AXIOS.get("/getRoleByPerson/", {
+                params: {
+                  personEmail: response2.data.account.person.email
+                }
+              })
+                .then(response => {
+                  var id = response.data.id;
+                  AXIOS.get("/manager/" + id, {}, {})
+                    .then(response => {
+                      this.userRole = "Manager";
+                      sessionStorage.setItem("role", "manager");
+                    })
+                    .catch(e => {
+                      this.error = e;
+                    });
+                  AXIOS.get("/customer/" + id, {}, {})
+                    .then(response => {
+                      this.userRole = "Customer";
+                      sessionStorage.setItem("role", "customer");
+                    })
+                    .catch(e => {
+                      this.error = e;
+                    });
+                  AXIOS.get("/employee/" + id, {}, {})
+                    .then(response => {
+                      this.userRole = "Employee";
+                      sessionStorage.setItem("role", "employee");
+                      sessionStorage.setItem("employeeId", response.data.id)
+                    })
+                    .catch(e => {
+                      this.error = e;
+                    });
+                    this.$router.push({ path: `/Profile/${this.account.username}` });
+                    location.reload();
+                })
+                .catch(e => {
+                  this.error = e;
+                });
+              
             })
             .catch(e => {
               this.errorLogin = e.response.data;
@@ -85,56 +122,5 @@ export default {
         });
     },
 
-    data() {
-        return {
-            account: {},
-            accounts: [],
-            username: "",
-            password: "",
-            errorLogin: "",
-            response: []
-        };
-    },
-    methods: {
-        signUp: function() {
-            this.$router.push({ path: `/SignUp` });
-        },
-
-        login: function(username, password) {
-            AXIOS.get("/loginAccount/", {
-                    params: {
-                        username: username,
-                        password: password
-                    }
-                })
-                .then(response => {
-                    this.accounts.push(response.data);
-                    this.username = "";
-                    this.password = "";
-                    this.errorLogin = "";
-                    this.account = this.accounts[this.accounts.length - 1];
-                    sessionStorage.setItem("username", username);
-                    sessionStorage.setItem("validUser", true);
-
-                    AXIOS.get("/cart/getWithUsername/", {
-                        params: {
-                            username: sessionStorage.getItem("username"),
-                        }
-                    })
-                    .then(response => {
-                        sessionStorage.setItem("cartId", response.data.id);
-                        this.$router.push({ path: `/Profile/${this.account.username}` });
-                        location.reload();
-                    })
-                    .catch(e => {
-                        this.errorLogin = e.response.data;
-                    });
-
-                })
-                .catch(e => {
-                    this.errorLogin = e.response.data;
-                });
-        }
-    }
   }
 };
