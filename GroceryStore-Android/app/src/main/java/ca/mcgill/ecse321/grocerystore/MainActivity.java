159274ab2064b8673fbd6a5dd9c7eda34b69797d
aigiarnc <aigiarn.cheuk@gmail.com>
2022-04-08 10:49:48 -0400

    Update MainActivity.java

--- GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/MainActivity.java
+++ GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/MainActivity.java
@@ -1,25 +1,30 @@
 package ca.mcgill.ecse321.grocerystore;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import ca.mcgill.ecse321.grocerystore.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import org.json.JSONObject;
+
+import ca.mcgill.ecse321.grocerystore.databinding.ActivityMainBinding;
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final JSONObject currentCustomer = null;
+    private final JSONObject newCustomer = null;
+    private String error = null;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -43,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // initialize error message text view
+        refreshErrorMessage();
     }
 
     @Override
@@ -72,5 +80,62 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void signup(View v) {
+        final EditText emailInput = findViewById(R.id.email_input);
+        String email = emailInput.getText().toString();
+        final EditText phoneInput = findViewById(R.id.phone_input);
+        String phone = phoneInput.getText().toString();
+        final EditText firstNameInput = findViewById(R.id.firstname_input);
+        String firstName = firstNameInput.getText().toString();
+        final EditText lastNameInput = findViewById(R.id.lastname_input);
+        String lastName = lastNameInput.getText().toString();
+        final EditText addressInput = findViewById(R.id.address_input);
+        String address = addressInput.getText().toString();
+        final EditText usernameInput = findViewById(R.id.username_input);
+        String username = usernameInput.getText().toString();
+        final EditText passwordInput = findViewById(R.id.password_input);
+        String password = passwordInput.getText().toString();
+
+        RequestParams rp = new RequestParams();
+        rp.add("firstName", firstName);
+        rp.add("lastName", lastName);
+        rp.add("phoneNumber", phone);
+        rp.add("address", address);
+
+        HttpUtils.post("createPerson/" + email, rp, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
     }
 }
