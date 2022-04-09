c487d970ef92e95c597ce95ce184572ffdee9fb0
joeyxoay <zhi.koay@mail.mcgill.ca>
2022-04-08 20:23:13 -0400

    deleting BE

--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/Employee.java
+++
@@ -1,8 +0,0 @@
-package mcgill.ecse321.grocerystore.model;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Employee extends UserRole{
-
-}
--- GroceryStore-Backend/src/main/java/mcgill/ecse321/grocerystore/model/Manager.java
+++
@@ -1,8 +0,0 @@
-package mcgill.ecse321.grocerystore.model;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Manager extends UserRole{
-
-}
