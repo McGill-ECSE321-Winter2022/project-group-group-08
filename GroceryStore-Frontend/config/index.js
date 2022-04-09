6a15ab05da17e8137bd918038ceb343af5ecbf53
joeyxoay <zhi.koay@mail.mcgill.ca>
2022-04-08 20:38:43 -0400

    change host url for FE and BE

--- GroceryStore-Frontend/config/index.js
+++ GroceryStore-Frontend/config/index.js
@@ -4,9 +4,9 @@ var path = require("path");
 module.exports = {
     build: {
         env: require("./prod.env"),
-        host: "eventregistration-frontend-123.herokuapp.com",
+        host: "grocerystore-frontend2-22ws.herokuapp.com",
         port: 443,
-        backendHost: "eventregistration-backend-123.herokuapp.com",
+        backendHost: "grocerystore-backend2-22ws.herokuapp.com",
         backendPort: 443,
         index: path.resolve(__dirname, "../dist/index.html"),
         assetsRoot: path.resolve(__dirname, "../dist"),
