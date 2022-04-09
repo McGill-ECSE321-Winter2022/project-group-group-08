1dfa431050dba571f53df2e1115f6b920daa733d
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2022-04-09 11:18:06 -0400

    Update index.js

--- GroceryStore-Frontend/config/index.js
+++ GroceryStore-Frontend/config/index.js
@@ -23,13 +23,13 @@ module.exports = {
         // View the bundle analyzer report after build finishes:
         // `npm run build --report`
         // Set to `true` or `false` to always turn it on or off
-        bundleAnalyzerReport: process.env.npm_config_report
+        bundleAnalyzerReport: process.env.npm_config_report,
     },
     dev: {
         env: require("./dev.env"),
         host: "127.0.0.1",
         port: 8087,
-        backendHost: "127.0.0.1",
+        backendHost: "grocerystore-backend2-22ws.herokuapp.com",
         backendPort: 8080,
         autoOpenBrowser: true,
         assetsSubDirectory: "static",
@@ -40,6 +40,6 @@ module.exports = {
         // (https://github.com/webpack/css-loader#sourcemaps)
         // In our experience, they generally work as expected,
         // just be aware of this issue when enabling this option.
-        cssSourceMap: false
-    }
+        cssSourceMap: false,
+    },
 };
