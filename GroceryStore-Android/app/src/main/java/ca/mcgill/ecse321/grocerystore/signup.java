1ba380eb6ca4a4d7ab8fba0cc4ea961c3cb331ce
aigiarnc <aigiarn.cheuk@gmail.com>
2022-04-09 17:36:08 -0400

    Update signup.java

--- GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/signup.java
+++ GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/signup.java
@@ -1,14 +1,25 @@
 package ca.mcgill.ecse321.grocerystore;
 
 import android.os.Bundle;
-import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +33,7 @@ public class signup extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String error_msg = "";
 
     public signup() {
         // Required empty public constructor
@@ -52,20 +61,145 @@ public class signup extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // TODO: Rename and change types of parameters
+            String mParam1 = getArguments().getString(ARG_PARAM1);
+            String mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_signup, container, false);
 
-        TextView signin_link =(TextView) view.findViewById(R.id.signin_link);
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        TextView error = v.findViewById(R.id.error);
+
+        CheckBox terms_checkBox = v.findViewById(R.id.terms_checkBox);
+        Button signup_btn = v.findViewById(R.id.signup_btn);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (terms_checkBox.isChecked()) {
+
+                } else {
+                    error_msg += "Please check the Term of Service and Privacy Policy";
+                    error.setText(error_msg);
+                }
+            }
+        });
+
+
+        TextView signin_link = v.findViewById(R.id.signin_link);
         signin_link.setMovementMethod(LinkMovementMethod.getInstance());
 
-        return view;
+        return v;
+    }
+
+    public void signup(View v) {
+        final EditText emailInput = v.findViewById(R.id.email_input);
+        String email = emailInput.getText().toString();
+        final EditText phoneInput = v.findViewById(R.id.phone_input);
+        String phone = phoneInput.getText().toString();
+        final EditText firstNameInput = v.findViewById(R.id.firstname_input);
+        String firstName = firstNameInput.getText().toString();
+        final EditText lastNameInput = v.findViewById(R.id.lastname_input);
+        String lastName = lastNameInput.getText().toString();
+        final EditText addressInput = v.findViewById(R.id.address_input);
+        String address = addressInput.getText().toString();
+        final EditText usernameInput = v.findViewById(R.id.username_input);
+        String username = usernameInput.getText().toString();
+        final EditText passwordInput = v.findViewById(R.id.password_input);
+        String password = passwordInput.getText().toString();
+        final Switch inTownSwitch = v.findViewById(R.id.town_switch);
+        String inTown = String.valueOf(inTownSwitch.isChecked());
+        String totalPoints = "0";
+        String tier = "Bronze";
+        String ban = "false";
+        TextView error = v.findViewById(R.id.error);
+
+        RequestParams rp_person = new RequestParams();
+        rp_person.add("firstName", firstName);
+        rp_person.add("lastName", lastName);
+        rp_person.add("phoneNumber", phone);
+        rp_person.add("address", address);
+
+        HttpUtils.post("createPerson/" + email, rp_person, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error_msg += e.getMessage();
+                    error.setText(error_msg);
+                }
+            }
+
+            //@Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error_msg += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error_msg += e.getMessage();
+                }
+                error.setText(error_msg);
+            }
+        });
+
+        RequestParams rp_account = new RequestParams();
+        rp_account.add("password", password);
+        rp_account.add("inTown", inTown);
+        rp_account.add("totalPoints", totalPoints);
+        rp_account.add("personEmail", email);
+
+        HttpUtils.post("createAccount/" + username, rp_account, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error_msg += e.getMessage();
+                    error.setText(error_msg);
+                }
+            }
+
+            //@Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error_msg += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error_msg += e.getMessage();
+                }
+                error.setText(error_msg);
+            }
+        });
+
+        RequestParams rp_customer = new RequestParams();
+        rp_customer.add("tierClass", tier);
+        rp_customer.add("ban", ban);
+        rp_customer.add("personEmail", email);
+
+        HttpUtils.post("customer/", rp_customer, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error_msg += e.getMessage();
+                    error.setText(error_msg);
+                }
+            }
+
+            //@Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error_msg += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error_msg += e.getMessage();
+                }
+                error.setText(error_msg);
+            }
+        });
     }
 }
